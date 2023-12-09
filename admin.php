a858f3e0ecec15ee03aec79aa9af9f7bdfb98ae2
kenzamouktabil <kenzamouktabil@gmail.com>
2023-12-09 18:46:34 +0100

    salma

--- admin.php
+++ admin.php
@@ -1,13 +1,15 @@
+
 <?php
 // Inclure le fichier de configuration pour la connexion à la base de données
 include('connexion.php');
 session_start();
 
 // Vérifier si l'utilisateur est connecté et a le rôle d'administrateur
-if (!isset($_SESSION['user_logged_in']) && $_SESSION['user_logged_in'] === false ) {
+if (!isset($_SESSION['user_logged_in']) || $_SESSION['user_logged_in'] !== true ) {
     // Rediriger vers la page d'accueil si l'utilisateur n'est pas connecté en tant qu'administrateur
     header('Location: index.php');
-    exit();}
+    exit();
+}
 
 // Vérifier si le formulaire pour créer un nouvel événement est soumis
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -19,9 +21,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $lieu = $_POST['lieu'];
     $createur = $_POST['createur'];
 
+    // Gérer le téléchargement de la photo
+    $dossier = 'photo/';
+    $nom_photo = '';
+
+    if(isset($_FILES['photo']) && $_FILES['photo']['error'] === 0) {
+        $infosfichier = pathinfo($_FILES['photo']['name']);
+        $extension_upload = strtolower($infosfichier['extension']);
+        $extensions_autorisees = array('png', 'jpeg', 'jpg');
+
+        if (in_array($extension_upload, $extensions_autorisees)) {
+            $nom_photo = $titre . "." . $extension_upload;
+            $chemin_photo = $dossier . $nom_photo;
+
+            if (!move_uploaded_file($_FILES['photo']['tmp_name'], $chemin_photo)) {
+                exit("Problème dans le téléchargement de l'image, Réessayez");
+            }
+        } else {
+            exit("Erreur, Veuillez insérer une image svp (extensions autorisées: png, jpeg, jpg)");
+        }
+    }
+
     // Insérer les données dans la table evenements
-    $query = "INSERT INTO evenement (date_debut,lieu,date_fin,description,titre,photo,createur) 
-              VALUES ('$date_debut', '$lieu', '$date_fin', '$description', '$titre', '$photo','$createur')";
+    $query = "INSERT INTO `evenement` (`date_debut`, `lieu`, `date_fin`, `description`, `titre`, `photo`, `createur`) 
+              VALUES ('$date_debut', '$lieu', '$date_fin', '$description', '$titre', '$nom_photo', '$createur')";
     $result = mysqli_query($link, $query);
 
     // Vérifier les erreurs de la requête
@@ -52,7 +75,8 @@ if (!$result) {
     <h1>Gestion des événements</h1>
 
     <!-- Formulaire pour créer de nouveaux événements -->
-    <form action="admin.php" method="post">
+    <form action="admin.php" method="post" enctype="multipart/form-data"> 
+    
         <label for="titre">Titre :</label>
         <input type="text" name="titre" required>
 
@@ -63,7 +87,7 @@ if (!$result) {
         <input type="date" name="date_debut" required>
 
         <label for="date">Date de fin :</label>
-        <input type="date" name="date_fin" required>
+        <input type="date" name="date_fin" >
 
 
         <label for="lieu">Lieu :</label>
@@ -73,10 +97,11 @@ if (!$result) {
         <input type="text" name="createur" required>
 
         <label for="photo">Photo:</label>
-        <input type="text" name="createur" required>
+        <input type="file" name="photo" required>
 
         <button type="submit">Créer un événement</button>
     </form>
+ 
 
     <!-- Liste des événements existants avec des liens pour les éditer ou les supprimer -->
     <h2>Liste des événements existants</h2>
@@ -84,7 +109,7 @@ if (!$result) {
         <?php
         while ($row = mysqli_fetch_assoc($result)) {
             echo "<li>";
-            echo "{$row['titre']} ({$row['date']}) - <a href='edit_event.php?id={$row['id']}'>Éditer</a> | <a href='delete_event.php?id={$row['id']}'>Supprimer</a>";
+            echo "{$row['titre']} ({$row['date_debut']}) - <a href='edit_event.php?id={$row['id_evenement']}'>Éditer</a> | <a href='delete_event.php?id={$row['id_evenement']}'>Supprimer</a>";
             echo "</li>";
         }
         ?>
@@ -95,7 +120,7 @@ if (!$result) {
     mysqli_free_result($result);
 
     // Fermer la connexion à la base de données
-    mysqli_close($conn);
+    mysqli_close($link);
     ?>
 </body>
 </html>
